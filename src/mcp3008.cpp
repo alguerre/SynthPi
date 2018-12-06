daf5e08a5f25006e7f934ba2f9994a9b843d1a9d
alguerre <alguerre@ucm.es>
2018-12-06 02:21:51 +0100

    Refactor

--- src/mcp3008.cpp
+++ src/mcp3008.cpp
@@ -1,63 +1,63 @@
 #include "mcp3008.h"
 
 
-SpiMcp3008::SpiMcp3008(int siSpiChannel){
-    this->setup(siSpiChannel);
+SpiMcp3008::SpiMcp3008(int si_spi_channel) {
+  this->Setup(si_spi_channel);
 }
 
 
-SpiMcp3008::~SpiMcp3008(){
+SpiMcp3008::~SpiMcp3008() {
 }
 
 
-SpiMcp3008* SpiMcp3008::pobSpi = NULL;
+SpiMcp3008* SpiMcp3008::pob_spi = NULL;
 
 
-SpiMcp3008* SpiMcp3008::get_instance(int siSpiChannel){
-    if (pobSpi == NULL)
-        pobSpi = new SpiMcp3008(siSpiChannel);
-    else
-        std::cout << "Singleton error" << std::endl;
-    return pobSpi;
+SpiMcp3008* SpiMcp3008::GetInstance(int si_spi_channel) {
+  if (pob_spi == NULL)
+    pob_spi = new SpiMcp3008(si_spi_channel);
+  else
+    std::cout << "Singleton error" << std::endl;
+  return pob_spi;
 }
 
 
-bool SpiMcp3008::setup(int siSpiChannel){
-    /*
-    SETUP initialize the SPI channel in RPi for analog reads.
-    */
-    this->siSpiChannel = siSpiChannel;
+bool SpiMcp3008::Setup(int si_spi_channel) {
+  /*
+  SETUP initialize the SPI channel in RPi for analog reads.
+  */
+  this->si_spi_channel = si_spi_channel;
 
-    if ((siSpiChannel != 0) && (siSpiChannel != 1))
-        return false;
+  if ((si_spi_channel != 0) && (si_spi_channel != 1))
+    return false;
 
-    if (wiringPiSPISetup(siSpiChannel, this->csiSpiSpeed) < 0)
-        return false;
-    
-    return true;
+  if (wiringPiSPISetup(si_spi_channel, k_spi_speed) < 0)
+    return false;
+
+  return true;
 }
 
 
-int SpiMcp3008::analog_read(const int csiChipChannel){
-    /*
-    ANALOGREAD reads analog values previously transform to digital by 
-    chip mcp3008. Three values needs to be sent to chip as data input:
-    bit start (1 bit), sgl/diff mode (1 bit) and channel (3 bits). Thus, data
-    input is formed by three bytes as:
-    [0-0-0-0-0-0-0-StartBit]-[s/d-D2-D1-D0-x-x-x-x]-[x-x-x-x-x-x-x-x]
+int SpiMcp3008::AnalogRead(const int si_chip_channel) {
+  /*
+  ANALOGREAD reads analog values previously transform to digital by
+  chip mcp3008. Three values needs to be sent to chip as data input:
+  bit start (1 bit), sgl/diff mode (1 bit) and channel (3 bits). Thus, data
+  input is formed by three bytes as:
+  [0-0-0-0-0-0-0-StartBit]-[s/d-D2-D1-D0-x-x-x-x]-[x-x-x-x-x-x-x-x]
 
-    Ouput is:
-    [?-?-?-?-?-?-?-?]-[?-?-?-?-?-0-B9-B8]-[B7-B6-B5-B4-B3-B2-B1-B0]
+  Ouput is:
+  [?-?-?-?-?-?-?-?]-[?-?-?-?-?-0-B9-B8]-[B7-B6-B5-B4-B3-B2-B1-B0]
 
-    where ? is high impedance and x don't care bits.
-    */
-    unsigned char pucSpiData[3];
+  where ? is high impedance and x don't care bits.
+  */
+  unsigned char puc_spi_data[3];
 
-    pucSpiData[0] = 0x1;
-    pucSpiData[1] = 0x80 | (csiChipChannel << 4);
-    pucSpiData[2] = 0x0; 
-    
-    wiringPiSPIDataRW (this->siSpiChannel, pucSpiData, 3) ;
+  puc_spi_data[0] = 0x1;
+  puc_spi_data[1] = 0x80 | (si_chip_channel << 4);
+  puc_spi_data[2] = 0x0;
 
-    return ((pucSpiData [1] << 8) | pucSpiData [2]) & 0x3FF ;
+  wiringPiSPIDataRW(this->si_spi_channel, puc_spi_data, 3);
+
+  return ((puc_spi_data[1] << 8) | puc_spi_data[2]) & 0x3FF;
 }
--- src/mcp3008.h
+++ src/mcp3008.h
@@ -1,16 +1,20 @@
 #include<iostream>
 #include<wiringPiSPI.h>
+#include "constants.h"
+#ifndef _MCP3008_h
+
+#define _MCP3008_h
 
 class SpiMcp3008 {
 private:
-    static SpiMcp3008* pobSpi;
-    const int csiSpiSpeed = 1000000;
-    int siSpiChannel = -1;
-    bool setup(int);
+    static SpiMcp3008* pob_spi;
+    int si_spi_channel = -1;
+    bool Setup(int);
 public:
     SpiMcp3008(int);
-    int analog_read(const int);
-    static SpiMcp3008 *get_instance(int);
+    int AnalogRead(const int);
+    static SpiMcp3008 *GetInstance(int);
     ~SpiMcp3008();
 };
 
+#endif
